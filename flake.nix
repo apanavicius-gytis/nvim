1bbf613870d08a3ad3809c4bc01c737794f05672
Gytis Apanavicius <gytis.apanavicius@ovoko.com>
2024-11-10 12:51:16 +0200

    flake

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Neovim config";
+  description = "My own Neovim flake";
   inputs = {
     nixpkgs = {
       url = "github:NixOS/nixpkgs";
@@ -9,11 +9,5 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { self, nixpkgs, neovim }: {
-      packages.x86_64-linux.default = neovim.packages.x86_64-linux.neovim;
-      apps.x86_64-linux.default = {
-        type = "app";
-        program = "${neovim.packages.x86_64-linux.neovim}/bin/nvim";
-      };
-    };
+  outputs = { self, nixpkgs, neovim }: { };
 }
