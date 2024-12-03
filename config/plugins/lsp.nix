2d7b13211188c99fe236aceffdd1fd0138332c2f
Gytis Apanavicius <gytis.apanavicius@ovoko.com>
2024-12-03 08:32:58 +0200

    change k8s file-name pattern

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -33,7 +33,7 @@
             settings = {
               yaml = {
                 schemas = {
-                  # kubernetes = "'*.yaml";
+                   kubernetes = "'*deployment*.yaml";
                   "http://json.schemastore.org/github-workflow" = ".github/workflows/*";
                   "http://json.schemastore.org/github-action" = ".github/action.{yml,yaml}";
                   "http://json.schemastore.org/ansible-stable-2.9" = "roles/tasks/*.{yml,yaml}";
