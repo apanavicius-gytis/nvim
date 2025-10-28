92d601ae65772499022286b317fc9dd3ad714b9b
Gytis Apanavicius <gytis.apanavicius@ovoko.com>
2025-10-28 16:20:12 +0200

    mb lol

--- config/plugins/debug.nix
+++ config/plugins/debug.nix
@@ -3,6 +3,8 @@
   plugins.dap.extensions.dap-go = {
     enable = true;
     delve.path = "${pkgs.delve}/bin/dlv";
-    delve.args = "--check-go-version=false";
+    delve.args = [
+      "--check-go-version=false"
+    ];
   };
 }
